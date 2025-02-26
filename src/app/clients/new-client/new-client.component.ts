d95fc14ce9afa4d8125dd094037e5bc0172b46bc
matheusmgs22 <matheusmgs17@gmail.com>
2025-02-26 15:57:39 -0300

    criando a página de cadastro de Clientes

--- src/app/clients/new-client/new-client.component.ts
+++ src/app/clients/new-client/new-client.component.ts
@@ -1,20 +1,38 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnDestroy } from '@angular/core';
 import { SERVICES_TOKEN } from '../../services/service.token';
 import { ICLientService } from '../../services/api-client/clients/iclients.service';
 import { ClientsService } from '../../services/api-client/clients/clients.service';
-
+import { ClientFormComponent } from "../components/client-form/client-form.component";
+import { Subscription } from 'rxjs';
+import { ClientModelForm } from '../client.models';
 
 @Component({
   selector: 'app-new-client',
-  imports: [],
+  imports: [ClientFormComponent],
   templateUrl: './new-client.component.html',
   styleUrl: './new-client.component.scss',
   providers: [
     { provide: SERVICES_TOKEN.HTTP.CLIENT, useClass: ClientsService }
   ]
 })
-export class NewClientComponent {
+export class NewClientComponent implements OnDestroy {
+[x: string]: any;
+
+  private httpSubscription?: Subscription
 
   constructor(
     @Inject(SERVICES_TOKEN.HTTP.CLIENT) private readonly httpService: ICLientService) { }
+
+
+    ngOnDestroy(): void {
+      if (this.httpSubscription) {
+        this.httpSubscription.unsubscribe()
+      }
+    }
+
+    onSubmitClient(value: ClientModelForm) {
+      const { id, ...request } = value
+      this.httpSubscription = this.httpService.save(request).subscribe(_ => {
+      })
+    }
 }
