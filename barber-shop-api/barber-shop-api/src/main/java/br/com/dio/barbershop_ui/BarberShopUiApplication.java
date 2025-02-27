90279097bae0560bc574011c5ce60fe97852be5a
matheusmgs22 <matheusmgs17@gmail.com>
2025-02-27 14:06:56 -0300

    Criando API barber shop

---
+++ barber-shop-api/barber-shop-api/src/main/java/br/com/dio/barbershop_ui/BarberShopUiApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio.barbershop_ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberShopUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberShopUiApplication.class, args);
+	}
+
+}
